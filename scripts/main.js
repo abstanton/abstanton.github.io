a11bd5a9d849c04630057ba1956cacf69232c10a
Angus Stanton <angusstanton225@gmail.com>
2021-08-20 16:06:45 +0100

    First commit for website, basic template completed. Planned features: - more on the about section - update cv properly - add some interactivity somewhere. Potentially a game with leaderboard? - add more links to github repos

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+var cv_button = document.getElementById('expand-button');
+var expand_arrow = document.getElementById('expand-arrow')
+var cv = document.getElementById("cv-section");
+
+cv_button.addEventListener("click", () => {
+    expand_arrow.classList.toggle("expand-arrow-active");
+    cv.classList.toggle("cv-section-active");
+})
