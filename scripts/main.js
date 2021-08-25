aa7a1c27250e8f5086d2dcbd9cd2b75de96c9297
Angus Stanton <angusstanton225@gmail.com>
2021-08-25 16:54:56 +0100

    added dark mode, fixed typo in good news description

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+document.body.style.zoom = "100%"
+
 var cv_button = document.getElementById('expand-button');
 var expand_arrow = document.getElementById('expand-arrow')
 var cv = document.getElementById("cv-section");
@@ -6,3 +8,6 @@ cv_button.addEventListener("click", () => {
     expand_arrow.classList.toggle("expand-arrow-active");
     cv.classList.toggle("cv-section-active");
 })
+
+var theme_toggle = document.querySelector("#theme-toggle")
+theme_toggle.addEventListener('change', ()=>{document.body.classList.toggle("dark_mode")})
